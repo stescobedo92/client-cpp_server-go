0d4b07f186026f86492790df9e5173bd6dedcc82
Sergio Triana Escobedo <stescobedo.31@gmail.com>
2023-11-23 11:22:49 -0600

    Adding validations and connection to the server

--- client/ClientCpp/ClientCpp/ClientCpp.cpp
+++ client/ClientCpp/ClientCpp/ClientCpp.cpp
@@ -6,5 +6,28 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        std::cerr << "Failed to initialize Winsock" << std::endl;
+        return -1;
+    }
+
+    SOCKET clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (clientSocket == INVALID_SOCKET) {
+        std::cerr << "Error creating socket: " << WSAGetLastError() << std::endl;
+        WSACleanup();
+        return -1;
+    }
+
+    sockaddr_in serverAddr;
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(8080);
+    serverAddr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+
+    if (connect(clientSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR) {
+        std::cerr << "Error connecting to server: " << WSAGetLastError() << std::endl;
+        closesocket(clientSocket);
+        WSACleanup();
+        return -1;
+    }
 }
